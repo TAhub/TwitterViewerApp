26992fe13a031cbe355f17320d4643c3a9178f6f
Theodore Abshire <theodoreabshire@macs-macbook-pro.local>
2015-10-26 14:34:43 -0700

    Initial commit! I made the table. Nothings in it yet, but it's there.

---
+++ TwitterApp/Tweet.swift
@@ -0,0 +1,15 @@
+//
+//  Tweet.swift
+//  TwitterApp
+//
+//  Created by Theodore Abshire on 10/26/15.
+//  Copyright © 2015 Theodore Abshire. All rights reserved.
+//
+
+import Foundation
+
+struct Tweet
+{
+	let name:String
+	let contents:String
+}
---
+++ TwitterApp/TweetTableViewController.swift
@@ -0,0 +1,40 @@
+//
+//  TweetTableViewController.swift
+//  TwitterApp
+//
+//  Created by Theodore Abshire on 10/26/15.
+//  Copyright © 2015 Theodore Abshire. All rights reserved.
+//
+
+import UIKit
+
+class TweetTableViewController: UITableViewController {
+
+	private var tweets:[Tweet]! = [Tweet]()
+	{
+		didSet
+		{
+			tableView.reloadData()
+		}
+	}
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int { return 1 }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tweets.count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("TweetCell", forIndexPath: indexPath)
+
+		cell.textLabel!.text = tweets[indexPath.row].name
+		cell.detailTextLabel!.text = tweets[indexPath.row].contents
+
+        return cell
+    }
+	
+	private func loadNewTweets()
+	{
+		
+	}
+}
